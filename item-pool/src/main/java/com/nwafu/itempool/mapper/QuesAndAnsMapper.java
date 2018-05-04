8e588b847e344f158a55ec7ebb708ed58c08ec85
tinchiway <hebeitianzhiwei@outlook.com>
2018-05-04 19:18:22 +0800

    return getquestions

--- item-pool/src/main/java/com/nwafu/itempool/mapper/QuesAndAnsMapper.java
+++ item-pool/src/main/java/com/nwafu/itempool/mapper/QuesAndAnsMapper.java
@@ -18,4 +18,6 @@ public interface QuesAndAnsMapper {
     int updateByPrimaryKeySelective(QuesAndAns record);
 
     int updateByPrimaryKey(QuesAndAns record);
+
+    List<QuesAndAns> selectAll();
 }
---
+++ organizepaper/src/main/java/com/nwafu/organizepaper/service/TrueOrFalseService.java
@@ -0,0 +1,22 @@
+package com.nwafu.organizepaper.service;
+
+import com.nwafu.itempool.mapper.TrueOrFalseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author TinChiWay
+ * @data 2018/5/4
+ */
+@Service
+public class TrueOrFalseService {
+    @Autowired
+    private TrueOrFalseMapper trueOrFalseMapper;
+
+    public List getTrueOrFalseQuestion(){
+        return trueOrFalseMapper.selectAll();
+    }
+
+}
